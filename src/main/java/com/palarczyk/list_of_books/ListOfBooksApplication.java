56ad23917973506f17bea0c50359cd7355c05829
sebastianpalarczyk <s.palarczyk12@gmail.com>
2021-07-18 13:03:34 +0200

    createProject

---
+++ src/main/java/com/palarczyk/list_of_books/ListOfBooksApplication.java
@@ -0,0 +1,13 @@
+package com.palarczyk.list_of_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListOfBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListOfBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palarczyk/list_of_books/ListOfBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palarczyk.list_of_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListOfBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
